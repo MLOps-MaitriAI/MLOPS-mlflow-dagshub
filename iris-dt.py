080147804910ba52e5acb42d20957b497083565a
MLOps-MaitriAI <hayyanshah111@gmail.com>
2024-07-27 11:21:34 +0530

    new

--- iris-dt.py
+++ iris-dt.py
@@ -17,7 +17,7 @@ data = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(
     data.data, data.target, test_size=0.2, random_state=42)
 
-max_depth=40
+max_depth=20
 
 mlflow.set_experiment('iris')
 # Log model hyperparameters and metrics to the MLflow server
